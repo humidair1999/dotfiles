b17e77efd48045f136288f3ec0ba9a911a73d17b
J. Ky Marsh <jkymarsh@gmail.com>
2018-03-16 10:49:31 -0700

    update installation script

--- install.sh
+++ install.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to the local dotfiles dir
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bash_profile bash_prompt bashrc aliases git-completion"    # list of files/folders to symlink in homedir
 
-##########
+########## Installation
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+mkdir -pv $olddir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv -v ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $PWD/$file ~/.$file
+    ln -sv $PWD/$file ~/.$file
 done
